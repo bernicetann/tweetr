ce3cc22abcc48aa840082ba0c6681802fdb8f37b
Bernice Tan <bernice.tann@gmail.com>
2017-05-17 18:58:29 +0000

    Implemented character counter on tweet container

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,21 @@
+$(function() {
+
+  const MAX_TWEET_LENGTH = 140;
+
+  $("#compose").on('keyup', function onTextAreaKeyUp() {
+    var $this = $(this);
+    var $counter = $this.siblings(".counter");
+
+    var currentCount = $this.val().length;
+    var charactersRemaining = MAX_TWEET_LENGTH - currentCount;
+
+    $counter.text(charactersRemaining);
+    if(charactersRemaining < 0) {
+      $counter.addClass("exceedCount");
+    } else {
+      $counter.removeClass("exceedCount");
+    }
+  });
+
+});
+
